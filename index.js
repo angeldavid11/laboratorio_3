6ff39ce9a1742bf27a0ce4a81bbce474c7163427
angeldavid11 <53090924+angeldavid11@users.noreply.github.com>
2019-08-03 18:47:12 -0600

    Create index.js
    el git no me corre en visual estudio por eso subo los archivos asi

---
+++ index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+const PORT = 80;
+let app = express();
+app.listen(PORT, function(){
+    console.log("My http server listening on port " + PORT + "...");
+});
+app.get('/foo', function(req, res){
+    console.log('Hello, I am foo.');
+});
